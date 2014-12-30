f0b8dd9608b7f4aa20ce6c759eb74ae79bd7e1c0
chenqi <chenqi@taotaosou.com>
2014-12-30 16:38:07 +0800

    updata tk_define

--- tasks/handlebars.js
+++ tasks/handlebars.js
@@ -7,137 +7,160 @@
  */
 
 'use strict';
+var chalk = require('chalk');
+var nsdeclare = require('nsdeclare');
 
 module.exports = function(grunt) {
-	var _ = grunt.util._;
-	var helpers = require('grunt-lib-contrib').init(grunt);
+  var _ = grunt.util._;
 
-	// content conversion for templates
-	var defaultProcessContent = function(content) { return content; };
+  // content conversion for templates
+  var defaultProcessContent = function(content) { return content; };
 
-	// AST processing for templates
-	var defaultProcessAST = function(ast) { return ast; };
+  // AST processing for templates
+  var defaultProcessAST = function(ast) { return ast; };
 
-	// filename conversion for templates
-	var defaultProcessName = function(name) { return name; };
+  // filename conversion for templates
+  var defaultProcessName = function(name) { return name; };
 
-	// filename conversion for partials
-	var defaultProcessPartialName = function(filePath) {
-		var pieces = _.last(filePath.split('/')).split('.');
-		var name   = _(pieces).without(_.last(pieces)).join('.'); // strips file extension
-		if (name.charAt(0) === '_') {
-			name = name.substr(1, name.length); // strips leading _ character
-		}
-		return name;
-	};
+  // filename conversion for partials
+  var defaultProcessPartialName = function(filepath) {
+    var pieces = _.last(filepath.split('/')).split('.');
+    var name   = _(pieces).without(_.last(pieces)).join('.'); // strips file extension
+    if (name.charAt(0) === '_') {
+      name = name.substr(1, name.length); // strips leading _ character
+    }
+    return name;
+  };
 
-	grunt.registerMultiTask('handlebars', 'Compile handlebars templates and partials.', function() {
-		var options = this.options({
-			namespace: 'JST',
-			separator: grunt.util.linefeed + grunt.util.linefeed,
-			wrapped: true,
-			amd: false,
-			commonjs: false,
-			knownHelpers: [],
-			knownHelpersOnly: false
-		});
-		grunt.verbose.writeflags(options, 'Options');
+  grunt.registerMultiTask('handlebars', 'Compile handlebars templates and partials.', function() {
+    var options = this.options({
+      namespace: 'JST',
+      separator: grunt.util.linefeed + grunt.util.linefeed,
+      wrapped: true,
+      amd: false,
+      commonjs: false,
+      knownHelpers: [],
+      knownHelpersOnly: false
+    });
 
-		var nsInfo;
-		if (options.namespace !== false) {
-			nsInfo = helpers.getNamespaceDeclaration(options.namespace);
-		}
+    // assign regex for partials directory detection
+    var partialsPathRegex = options.partialsPathRegex || /./;
 
-		// assign regex for partials directory detection
-		var partialsPathRegex = options.partialsPathRegex || /./;
+    // assign regex for partial detection
+    var isPartialRegex = options.partialRegex || /^_/;
 
-		// assign regex for partial detection
-		var isPartial = options.partialRegex || /^_/;
+    // assign transformation functions
+    var processContent = options.processContent || defaultProcessContent;
+    var processName = options.processName || defaultProcessName;
+    var processPartialName = options.processPartialName || defaultProcessPartialName;
+    var processAST = options.processAST || defaultProcessAST;
+    var useNamespace = options.namespace !== false;
 
-		// assign transformation functions
-		var processContent = options.processContent || defaultProcessContent;
-		var processName = options.processName || defaultProcessName;
-		var processPartialName = options.processPartialName || defaultProcessPartialName;
-		var processAST = options.processAST || defaultProcessAST;
+    // assign compiler options
+    var compilerOptions = options.compilerOptions || {};
+    var filesCount = 0;
 
-		// assign compiler options
-		var compilerOptions = options.compilerOptions || {};
+    this.files.forEach(function(f) {
+      var partials = [];
+      var templates = [];
 
-		this.files.forEach(function(f) {
-			var partials = [];
-			var templates = [];
+      // Namespace info for current template
+      var nsInfo;
 
-			// iterate files, processing partials and templates separately
-			f.src.filter(function(filepath) {
-				// Warn on and remove invalid source files (if nonull was set).
-				if (!grunt.file.exists(filepath)) {
-					grunt.log.warn('Source file "' + filepath + '" not found.');
-					return false;
-				} else {
-					return true;
-				}
-			})
-				.forEach(function(filepath) {
-					var src = processContent(grunt.file.read(filepath), filepath);
-					var Handlebars = require('handlebars');
-					var ast, compiled, filename;
-					try {
-						// parse the handlebars template into it's AST
-						ast = processAST(Handlebars.parse(src));
-						compiled = Handlebars.precompile(ast, compilerOptions);
+      // Map of already declared namespace parts
+      var nsDeclarations = {};
 
-						// if configured to, wrap template in Handlebars.template call
-						if (options.wrapped === true) {
-							compiled = 'Handlebars.template('+compiled+')';
-						}
-					} catch (e) {
-						grunt.log.error(e);
-						grunt.fail.warn('Handlebars failed to compile '+filepath+'.');
-					}
+      // nsdeclare options when fetching namespace info
+      var nsDeclareOptions = {response: 'details', declared: nsDeclarations};
 
-					// register partial or add template to namespace
-					if (partialsPathRegex.test(filepath) && isPartial.test(_.last(filepath.split('/')))) {
-						filename = processPartialName(filepath);
-						if (options.partialsUseNamespace === true) {
-							partials.push('Handlebars.registerPartial('+JSON.stringify(filename)+', '+nsInfo.namespace+'['+JSON.stringify(filename)+'] = '+compiled+');');
-						} else {
-							partials.push('Handlebars.registerPartial('+JSON.stringify(filename)+', '+compiled+');');
-						}
-					} else {
-						if(options.amd && options.namespace === false) {
-							compiled = 'return ' + compiled;
-						}
-						filename = processName(filepath);
-						if (options.namespace !== false) {
-							templates.push(nsInfo.namespace+'['+JSON.stringify(filename)+'] = '+compiled+';');
-						} else if (options.commonjs === true) {
-							templates.push('templates['+JSON.stringify(filename)+'] = '+compiled+';');
-						} else {
-							templates.push(compiled);
-						}
-					}
-				});
+      // Just get the namespace info for a given template
+      var getNamespaceInfo = _.memoize(function(filepath) {
+        if (!useNamespace) {return undefined;}
+        if (_.isFunction(options.namespace)) {
+          return nsdeclare(options.namespace(filepath), nsDeclareOptions);
+        } else {
+          return nsdeclare(options.namespace, nsDeclareOptions);
+        }
+      });
 
-			var output = partials.concat(templates);
-			if (output.length < 1) {
-				grunt.log.warn('Destination not written because compiled files were empty.');
-			} else {
-				if (options.namespace !== false) {
-					output.unshift(nsInfo.declaration);
+      // iterate files, processing partials and templates separately
+      f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      })
+      .forEach(function(filepath) {
+        var src = processContent(grunt.file.read(filepath), filepath);
 
-					if (options.node) {
-						output.unshift('Handlebars = glob.Handlebars || require(\'handlebars\');');
-						output.unshift('var glob = (\'undefined\' === typeof window) ? global : window,');
+        var Handlebars = require('handlebars');
+        var ast, compiled, filename;
+        try {
+          // parse the handlebars template into it's AST
+          ast = processAST(Handlebars.parse(src));
+          compiled = Handlebars.precompile(ast, compilerOptions);
 
-						var nodeExport = 'if (typeof exports === \'object\' && exports) {';
-						nodeExport += 'module.exports = ' + nsInfo.namespace + ';}';
+          // if configured to, wrap template in Handlebars.template call
+          if (options.wrapped === true) {
+            compiled = 'Handlebars.template(' + compiled + ')';
+          }
+        } catch (e) {
+          grunt.log.error(e);
+          grunt.fail.warn('Handlebars failed to compile ' + filepath + '.');
+        }
 
-						output.push(nodeExport);
-					}
+        // register partial or add template to namespace
+        if (partialsPathRegex.test(filepath) && isPartialRegex.test(_.last(filepath.split('/')))) {
+          filename = processPartialName(filepath);
+          if (options.partialsUseNamespace === true) {
+            nsInfo = getNamespaceInfo(filepath);
 
-				}
+            partials.push(nsInfo.declaration);
+            partials.push('Handlebars.registerPartial(' + JSON.stringify(filename) + ', ' + nsInfo.namespace +
+              '[' + JSON.stringify(filename) + '] = ' + compiled + ');');
+          } else {
+            partials.push('Handlebars.registerPartial(' + JSON.stringify(filename) + ', ' + compiled + ');');
+          }
+        } else {
+          nsInfo = getNamespaceInfo(filepath);
 
-				if (options.amd) {
+          if (options.amd && !useNamespace) {
+            compiled = 'return ' + compiled;
+          }
+          filename = processName(filepath);
+          if (useNamespace) {
+            templates.push(nsInfo.declaration);
+            templates.push(nsInfo.namespace + '[' + JSON.stringify(filename) + '] = ' + compiled + ';');
+          } else if (options.commonjs === true) {
+            templates.push('templates[' + JSON.stringify(filename) + '] = ' + compiled + ';');
+          } else {
+            templates.push(compiled);
+          }
+        }
+      });
+
+      var output = partials.concat(templates);
+      if (output.length < 1) {
+        grunt.log.warn('Destination not written because compiled files were empty.');
+      } else {
+        if (useNamespace) {
+          if (options.node) {
+            output.unshift('Handlebars = glob.Handlebars || require(\'handlebars\');');
+            output.unshift('var glob = (\'undefined\' === typeof window) ? global : window,');
+
+            var nodeExport = 'if (typeof exports === \'object\' && exports) {';
+            nodeExport += 'module.exports = ' + nsInfo.namespace + ';}';
+
+            output.push(nodeExport);
+          }
+
+        }
+
+        if (options.amd) {
+          // Wrap the file in an AMD define fn.
 					// Wrap the file in an AMD define fn.
 					output.unshift("__tk2__define(function (require, exports, module) {var Handlebars = require('./lib/handlebars');");
 
@@ -147,25 +170,28 @@ module.exports = function(grunt) {
 						output.push("return "+nsInfo.namespace+";");
 					}
 					output.push("});");
-				}
+        }
 
-				if (options.commonjs) {
-					if (options.namespace === false) {
-						output.unshift('var templates = {};');
-						output.push("return templates;");
-					} else {
-						output.push("return "+nsInfo.namespace+";");
-					}
-					// Export the templates object for CommonJS environments.
-					output.unshift("module.exports = function(Handlebars) {");
-					output.push("};");
-				}
+        if (options.commonjs) {
+          if (useNamespace) {
+            output.push('return ' + nsInfo.namespace + ';');
+          } else {
+            output.unshift('var templates = {};');
+            output.push('return templates;');
+          }
+          // Export the templates object for CommonJS environments.
+          output.unshift('module.exports = function(Handlebars) {');
+          output.push('};');
+        }
 
-				grunt.file.write(f.dest, output.join(grunt.util.normalizelf(options.separator)));
-				grunt.log.writeln('File "' + f.dest + '" created.');
-			}
-		});
+        filesCount++;
+        grunt.file.write(f.dest, output.join(grunt.util.normalizelf(options.separator)));
+        grunt.verbose.writeln('File ' + chalk.cyan(f.dest) + ' created.');
+      }
+    });
 
-	});
+    grunt.log.ok(filesCount + ' ' + grunt.util.pluralize(filesCount, 'file/files') + ' created.');
+
+  });
 
 };
