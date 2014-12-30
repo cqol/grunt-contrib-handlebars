67de3a588002174fb2485b51820ae7d8f8da0ab6
chenqi <chenqi@taotaosou.com>
2014-12-30 15:32:15 +0800

    updata tk_define

--- tasks/handlebars.js
+++ tasks/handlebars.js
@@ -161,31 +161,14 @@ module.exports = function(grunt) {
 
         if (options.amd) {
           // Wrap the file in an AMD define fn.
-          if (typeof options.amd === 'boolean') {
-            output.unshift('__tk2__define([\'handlebars\'], function(Handlebars) {');
-          } else if (typeof options.amd === 'string') {
-            output.unshift('__tk2__define([\'' + options.amd + '\'], function(Handlebars) {');
-          } else if (Array.isArray(options.amd)) {
-            // convert options.amd to a string of dependencies for require([...])
-            var amdString = '';
-            for (var i = 0; i < options.amd.length; i++) {
-              if (i !== 0) {
-                amdString += ', ';
-              }
-
-              amdString += '\'' + options.amd[i] + '\'';
-            }
-
-            // Wrap the file in an AMD define fn.
-            output.unshift('__tk2__define([' + amdString + '], function(Handlebars) {');
-          }
-
-          if (useNamespace) {
-            // Namespace has not been explicitly set to false; the AMD
-            // wrapper will return the object containing the template.
-            output.push('return ' + nsInfo.namespace + ';');
-          }
-          output.push('});');
+					// Wrap the file in an AMD define fn.
+					output.unshift("__tk2__define(function (require, exports, module) {var Handlebars = require('./lib/handlebars');");
+					if (options.namespace !== false) {
+						// Namespace has not been explicitly set to false; the AMD
+						// wrapper will return the object containing the template.
+						output.push("return "+nsInfo.namespace+";");
+					}
+					output.push("});");
         }
 
         if (options.commonjs) {
